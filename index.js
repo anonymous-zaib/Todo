d5cd6b3acf2f6a2a2d36c9931347dcd622695460
shahzaib <muhammadshahzaibqureshi497@gmail.com>
2025-01-23 12:34:00 +0500

    cors changed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = 3000
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173"]  
+    origin: ["http://localhost:3000"]  
 }))
 app.use(bodyParser.json());
 app.use('/users', userRoute);
